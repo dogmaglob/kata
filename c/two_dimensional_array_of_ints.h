ddd4a7052cae1c07f7546dfffaae3835f9adc4ed
Kelley Rogers <kelro@cisco.com>
2014-07-15 10:17:29 -0700

    First commit with folder for C kata.

---
+++ c/two_dimensional_array_of_ints.h
@@ -0,0 +1,9 @@
+int ** create_two_dimensional_array_of_ints(int number_of_rows,
+                                            int number_of_columns);
+void print_two_dimensional_array_of_ints(int ** array,
+                                         int number_of_rows,
+                                         int number_of_columns);
+void free_two_dimensional_array_of_ints(int ** array,
+                                        int number_of_rows,
+                                        int number_of_columns);
+
